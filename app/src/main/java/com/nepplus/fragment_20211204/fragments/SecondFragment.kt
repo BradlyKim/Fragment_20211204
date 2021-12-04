70fe3462f91c057fd4dbc21e678316a17fa88a30
BradlyKim <kjh5864@naver.com>
2021-12-04 12:18:41 +0900

    두번째 Fragment 연습문제 작성

---
+++ app/src/main/java/com/nepplus/fragment_20211204/fragments/SecondFragment.kt
@@ -0,0 +1,33 @@
+package com.nepplus.fragment_20211204.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import com.nepplus.fragment_20211204.R
+import kotlinx.android.synthetic.main.fragment_second.*
+
+class SecondFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_second, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        
+        btnToast2.setOnClickListener {
+
+            Toast.makeText(requireContext(), "두번째 Fragment에서 Toast 띄우기", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+
+
+}
